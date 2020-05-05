32fb7305df5975a02a22b64e0fc50d132d8cb3f1
ruan4261 <ruan4261@foxmail.com>
2020-05-05 19:50:27 +0800

    20-5-5

---
+++ leetcode/q502/Solution2.java
@@ -0,0 +1,42 @@
+package com.ruan.alg.leetcode.leetcode.q502;
+
+import java.util.PriorityQueue;
+
+public class Solution2 {
+
+    public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
+        // 启动资金充足情况下的提速方案
+        boolean speedUp = true;
+        for (int c: Capital) if (W < c) speedUp = false;
+        if (speedUp) {
+            PriorityQueue<Integer> heap = new PriorityQueue<>();
+            for (int p: Profits) {
+                heap.add(p);
+                if (heap.size() > k) heap.poll();
+            }
+            for (int h: heap) W += h;
+            return W;
+        }
+
+        int idx;
+        int n = Profits.length;
+        // 每个项目只能选择一次，此循环为所有项目被选择的总次数
+        for(int i = 0; i < Math.min(k, n); ++i) {
+            idx = -1;
+            // 选择这次使用什么项目
+            for(int j = 0; j < n; ++j) {
+                if (W >= Capital[j]) {
+                    if (idx == -1 ) idx = j;
+                    else if (Profits[idx] < Profits[j]) idx = j;
+                }
+            }
+            // 没有选择项目，则当前启动资金已经不足以启动任何项目
+            if(idx == -1) break;
+
+            // 加上本次项目的利润，然后将本次启动项目的启动资金设为MAX，其将不会再被选择
+            W += Profits[idx];
+            Capital[idx] = Integer.MAX_VALUE;
+        }
+        return  W;
+    }
+}
---
+++ leetcode/q98/Solution.java
@@ -0,0 +1,163 @@
+package com.ruan.alg.leetcode.leetcode.q98;
+
+/**
+ * 98. 验证二叉搜索树
+ * <p>
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 假设一个二叉搜索树具有如下特征：
+ * <p>
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * 2
+ * / \
+ * 1   3
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:
+ * 5
+ * / \
+ * 1   4
+ *      / \
+ *     3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ *      根节点的值为 5 ，但是其右子节点值为 4 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ruan4261
+ */
+class Solution {
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        /*if (root.left != null) {
+            if (root.left.val >= root.val) return false;
+            if (!verifyOnlyMax(root.left, root.val)) return false;
+        }
+        if (root.right != null) {
+            if (root.right.val <= root.val) return false;
+            if (!verifyOnlyMin(root.right, root.val)) return false;
+        }*/
+        if (root.left != null) {
+            if (root.left.val >= root.val) {
+                return false;
+            }
+            if (!verify(root.left, null, root.val)) return false;
+        }
+        if (root.right != null) {
+            if (root.right.val <= root.val) {
+                return false;
+            }
+            if (!verify(root.right, root.val, null)) return false;
+        }
+        return true;
+    }
+
+    /**
+     * 独立可运行
+     *
+     * @param treeNode
+     * @param min
+     * @param max
+     * @return
+     */
+    public boolean verify(TreeNode treeNode, Integer min, Integer max) {
+        if (treeNode.left != null) {
+            if (treeNode.left.val >= treeNode.val || (min != null && treeNode.left.val <= min))
+                return false;
+            if (!verify(treeNode.left, min, Integer.valueOf(treeNode.val))) return false;
+        }
+        if (treeNode.right != null) {
+            if (treeNode.right.val <= treeNode.val || (max != null && treeNode.right.val >= max))
+                return false;
+            if (!verify(treeNode.right, Integer.valueOf(treeNode.val), max)) return false;
+        }
+        return true;
+    }
+
+    /**
+     * 验证树区间
+     *
+     * @param treeNode
+     * @param min      区间左极限
+     * @param max      区间右极限
+     * @return
+     */
+    public boolean verify(TreeNode treeNode, int min, int max) {
+        if (treeNode.left != null) {
+            if (treeNode.left.val >= treeNode.val || treeNode.left.val <= min)
+                return false;
+            if (!verify(treeNode.left, min, treeNode.val)) return false;
+        }
+        if (treeNode.right != null) {
+            if (treeNode.right.val <= treeNode.val || treeNode.right.val >= max)
+                return false;
+            if (!verify(treeNode.right, treeNode.val, max)) return false;
+        }
+        return true;
+    }
+
+    /**
+     * 其实是重载
+     * 无右区间限制
+     *
+     * @param treeNode
+     * @param min
+     * @return
+     */
+    public boolean verifyOnlyMin(TreeNode treeNode, int min) {
+        if (treeNode.left != null) {
+            if (treeNode.left.val >= treeNode.val || treeNode.left.val <= min)
+                return false;
+            if (!verify(treeNode.left, min, treeNode.val)) return false;
+        }
+        if (treeNode.right != null) {
+            if (treeNode.right.val <= treeNode.val)
+                return false;
+            if (!verifyOnlyMin(treeNode.right, treeNode.val)) return false;
+        }
+        return true;
+    }
+
+    /**
+     * 其实是重载
+     * 无左区间限制
+     *
+     * @param treeNode
+     * @param max
+     * @return
+     */
+    public boolean verifyOnlyMax(TreeNode treeNode, int max) {
+        if (treeNode.left != null) {
+            if (treeNode.left.val >= treeNode.val)
+                return false;
+            if (!verifyOnlyMax(treeNode.left, treeNode.val)) return false;
+        }
+        if (treeNode.right != null) {
+            if (treeNode.right.val <= treeNode.val || treeNode.right.val >= max)
+                return false;
+            if (!verify(treeNode.right, treeNode.val, max)) return false;
+        }
+        return true;
+    }
+}
