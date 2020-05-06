f52a186fde4dacf82e50de041f86f05281b67c78
ruan4261 <ruan4261@foxmail.com>
2020-05-06 13:47:27 +0800

    20-5-6

---
+++ leetcode/q1400/Solution.java
@@ -0,0 +1,80 @@
+package com.ruan.alg.leetcode.leetcode.q1400;
+
+/**
+ * 1400. 构造 K 个回文字符串
+ * <p>
+ * 给你一个字符串 s 和一个整数 k 。请你用 s 字符串中 所有字符 构造 k 个非空 回文串 。
+ * <p>
+ * 如果你可以用 s 中所有字符构造 k 个回文字符串，那么请你返回 True ，否则返回 False 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "annabelle", k = 2
+ * 输出：true
+ * 解释：可以用 s 中所有字符构造 2 个回文字符串。
+ * 一些可行的构造方案包括："anna" + "elble"，"anbna" + "elle"，"anellena" + "b"
+ * 示例 2：
+ * <p>
+ * 输入：s = "leetcode", k = 3
+ * 输出：false
+ * 解释：无法用 s 中所有字符构造 3 个回文串。
+ * 示例 3：
+ * <p>
+ * 输入：s = "true", k = 4
+ * 输出：true
+ * 解释：唯一可行的方案是让 s 中每个字符单独构成一个字符串。
+ * 示例 4：
+ * <p>
+ * 输入：s = "yzyzyzyzyzyzyzy", k = 2
+ * 输出：true
+ * 解释：你只需要将所有的 z 放在一个字符串中，所有的 y 放在另一个字符串中。那么两个字符串都是回文串。
+ * 示例 5：
+ * <p>
+ * 输入：s = "cr", k = 7
+ * 输出：false
+ * 解释：我们没有足够的字符去构造 7 个回文串。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 10^5
+ * s 中所有字符都是小写英文字母。
+ * 1 <= k <= 10^5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/construct-k-palindrome-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ruan4261
+ */
+public class Solution {
+
+    /**
+     * 奇数个字符总数>k则无法构造
+     *
+     * @param s
+     * @param k
+     * @return
+     */
+    public boolean canConstruct(String s, int k) {
+        int len = s.length();
+        if (len == k) return true;
+        else if (len < k) return false;
+
+        int[] arr = new int[26];
+        for (char c : s.toCharArray()) {
+            arr[c - 'a']++;
+        }
+
+        int singleCount = 0;
+        for (int i : arr) {
+            if (i % 2 == 1) singleCount++;
+        }
+
+        if (singleCount > k) return false;
+        else return true;
+    }
+
+}
---
+++ leetcode/q2/Solution.java
@@ -0,0 +1,104 @@
+package com.ruan.alg.leetcode.leetcode.q2;
+
+/**
+ * 2. 两数相加
+ * <p>
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * <p>
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ruan4261
+ */
+class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = new ListNode(0);
+        dummy.next = curr;
+        boolean add = false;
+        while (l1 != null || l2 != null || add) {
+            int a = l1 == null ? 0 : l1.val;
+            int b = l2 == null ? 0 : l2.val;
+
+            int node = a + b;
+            if (add) {
+                node++;
+                add = false;
+            }
+            if (node > 9) {
+                curr.val = node % 10;
+                add = true;
+            } else {
+                curr.val = node;
+            }
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+
+            if (l1 != null || l2 != null || add) {
+                curr.next = new ListNode(-1);
+                curr = curr.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    /**
+     * 还踏马是3ms，破leetcode有问题
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode dummy_head = new ListNode(0);
+        ListNode l3 = dummy_head;
+        boolean carry = false;
+        while (l1 != null || l2 != null) {
+            int l1_val = (l1 != null) ? l1.val : 0;
+            int l2_val = (l2 != null) ? l2.val : 0;
+            int current_sum = l1_val + l2_val + (carry ? 1 : 0);
+            carry = false;
+            if (current_sum > 9) {
+                carry = true;
+                l3.next = new ListNode(current_sum % 10);
+            } else {
+                l3.next = new ListNode(current_sum);
+            }
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            l3 = l3.next;
+        }
+        if (carry) {
+            l3.next = new ListNode(1);
+        }
+        return dummy_head.next;
+    }
+
+}
