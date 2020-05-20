1bd11b6fb1f51f7179fa6cc9fc21488c39d13a25
ruan4261 <ruan4261@foxmail.com>
2020-05-20 17:57:00 +0800

    20-5-20

---
+++ leetcode/q1371/Solution.java
@@ -0,0 +1,69 @@
+package com.ruan.alg.leetcode.leetcode.q1371;
+
+import java.util.Arrays;
+
+/**
+ * 1371. 每个元音包含偶数次的最长子字符串
+ *
+ * 给你一个字符串 s ，请你返回满足以下条件的最长子字符串的长度：每个元音字母，即 'a'，'e'，'i'，'o'，'u' ，在子字符串中都恰好出现了偶数次。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "eleetminicoworoep"
+ * 输出：13
+ * 解释：最长子字符串是 "leetminicowor" ，它包含 e，i，o 各 2 个，以及 0 个 a，u 。
+ * 示例 2：
+ *
+ * 输入：s = "leetcodeisgreat"
+ * 输出：5
+ * 解释：最长子字符串是 "leetc" ，其中包含 2 个 e 。
+ * 示例 3：
+ *
+ * 输入：s = "bcbcbc"
+ * 输出：6
+ * 解释：这个示例中，字符串 "bcbcbc" 本身就是最长的，因为所有的元音 a，e，i，o，u 都出现了 0 次。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 5 x 10^5
+ * s 只包含小写英文字母。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-the-longest-substring-containing-vowels-in-even-counts
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+
+    public int findTheLongestSubstring(String s) {
+        int ans = 0, status = 0;
+        int n = s.length();
+        int[] pos = new int[1 << 5];
+        Arrays.fill(pos, -1);
+        pos[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch == 'a') {
+                status ^= 1;
+            } else if (ch == 'e') {
+                status ^= 2;
+            } else if (ch == 'i') {
+                status ^= 4;
+            } else if (ch == 'o') {
+                status ^= 8;
+            } else if (ch == 'u') {
+                status ^= 16;
+            }
+            if (pos[status] >= 0) {
+                ans = Math.max(ans, i + 1 - pos[status]);
+            } else {
+                pos[status] = i + 1;
+            }
+        }
+        return ans;
+    }
+
+}
