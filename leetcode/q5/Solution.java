5c1c3acb7d0e1f0e007868189f76a527b1c38b15
ruan4261 <ruan4261@foxmail.com>
2020-05-21 18:00:40 +0800

    20-5-21

---
+++ leetcode/q5/Solution.java
@@ -0,0 +1,58 @@
+package com.ruan.alg.leetcode.leetcode.q5;
+
+/**
+ * 5. 最长回文子串
+ * <p>
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ * <p>
+ * 输入: "cbbd"
+ * 输出: "bb"
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ruan4261
+ */
+public class Solution {
+
+    /**
+     * 暴力滑窗超时
+     *
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+        if (s == null || s.isEmpty()) return "";
+        String ans = String.valueOf(s.charAt(0));
+        int len = s.length();
+        int window = 2;
+        while (window <= len) {
+            for (int i = 0; i < len - window + 1; i++) {
+                String t = s.substring(i, i + window);
+                if (isPalindrome(t)) {
+                    ans = t;
+                    break;
+                }
+            }
+            window++;
+        }
+        return ans;
+    }
+
+    private boolean isPalindrome(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0, j = chars.length - 1; i < j; i++, j--) {
+            if (chars[i] != chars[j]) return false;
+        }
+        return true;
+    }
+
+}
---
+++ leetcode/q5/Solution2.java
@@ -0,0 +1,37 @@
+package com.ruan.alg.leetcode.leetcode.q5;
+
+/**
+ * try dp
+ */
+public class Solution2 {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 2) return s;
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        char[] chars = s.toCharArray();
+        int maxLen = 1;
+        int begin = 0;
+
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (chars[j] != chars[i]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+}
