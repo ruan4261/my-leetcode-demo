24e34074c830cda77d3b2e0b876167f7bda9e64d
ruan4261 <ruan4261@foxmail.com>
2020-05-22 14:07:06 +0800

    20-5-22

---
+++ leetcode/q105/Solution.java
@@ -0,0 +1,73 @@
+package com.ruan.alg.leetcode.leetcode.q105;
+
+import java.util.Stack;
+
+/**
+ * 105. 从前序与中序遍历序列构造二叉树
+ * <p>
+ * 根据一棵树的前序遍历与中序遍历构造二叉树。
+ * <p>
+ * 注意:
+ * 你可以假设树中没有重复的元素。
+ * <p>
+ * 例如，给出
+ * <p>
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 返回如下的二叉树：
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+
+    /**
+     *
+     * @param preorder 前序遍历
+     * @param inorder 中序遍历
+     * @return
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        int inorderIndex = 0;
+
+        for (int i = 1; i < preorder.length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
+        }
+
+        return root;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
---
+++ leetcode/q105/Solution2.java
@@ -0,0 +1,49 @@
+package com.ruan.alg.leetcode.leetcode.q105;
+
+/**
+ *         3
+ *        / \
+ *       9  20
+ *      /  /  \
+ *     8  15   7
+ *    / \
+ *   5  10
+ *  /
+ * 4
+ *
+ * preorder = [3, 9, 8, 5, 4, 10, 20, 15, 7]
+ * inorder = [4, 5, 8, 10, 9, 3, 15, 20, 7]
+ */
+public class Solution2 {
+
+    private int pre = 0, in = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return recursive(preorder, inorder, Integer.MAX_VALUE);
+    }
+
+    private TreeNode recursive(int[] preorder, int[] inorder, int stop) {
+        if (pre >= preorder.length) return null;
+        if (inorder[in] == stop) {
+            in++;
+            return null;
+        }
+
+        int curVal = preorder[pre++];
+        TreeNode cur = new TreeNode(curVal);
+        cur.left = recursive(preorder, inorder, curVal);
+        cur.right = recursive(preorder, inorder, stop);
+        return cur;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
