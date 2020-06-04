9d08d40c4e38b473aaf10d0b8ae25c0ed018c016
ruan4261 <ruan4261@foxmail.com>
2020-06-04 15:20:19 +0800

    20-6-4

---
+++ leetcode/q238/Solution.java
@@ -0,0 +1,47 @@
+package com.ruan.alg.leetcode.leetcode.q238;
+
+/**
+ * 238. 除自身以外数组的乘积
+ * <p>
+ * 给你一个长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [1,2,3,4]
+ * 输出: [24,12,8,6]
+ *  
+ * <p>
+ * 提示：题目数据保证数组之中任意元素的全部前缀元素和后缀（甚至是整个数组）的乘积都在 32 位整数范围内。
+ * <p>
+ * 说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ * <p>
+ * 进阶：
+ * 你可以在常数空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。）
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/product-of-array-except-self
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ruan4261
+ */
+public class Solution {
+
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[len];
+        int up = 1;
+        for (int i = 0; i < len; i++) {
+            up = ans[i] = nums[i] * up;
+        }
+        int down = 1;
+        for (int i = len - 1; i > 0; i--) {
+            ans[i] = ans[i - 1] * down;
+            down *= nums[i];
+        }
+        ans[0] = down;
+        return ans;
+    }
+
+}
