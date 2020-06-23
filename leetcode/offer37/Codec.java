6e172de54d8cbf015ed50e10cd7b5c616e9a43ad
ruan4261 <ruan4261@foxmail.com>
2020-06-23 16:29:42 +0800

    20-6-23

---
+++ leetcode/offer37/Codec.java
@@ -0,0 +1,58 @@
+package com.ruan.alg.leetcode.leetcode.offer37;
+
+/**
+ * 剑指 Offer 37. 序列化二叉树
+ * 此处整活，题解见@see
+ * <p>
+ * 请实现两个函数，分别用来序列化和反序列化二叉树。
+ * <p>
+ * 示例: 
+ * <p>
+ * 你可以将以下二叉树：
+ * <p>
+ * 1
+ * / \
+ * 2   3
+ * / \
+ * 4   5
+ * <p>
+ * 序列化为 "[1,2,3,null,null,4,5]"
+ * 注意：本题与主站 297 题相同：https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/xu-lie-hua-er-cha-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ruan4261
+ * @see com.ruan.alg.leetcode.leetcode.q297
+ */
+public class Codec {
+
+    private TreeNode node;
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        this.node = root;
+        return "hehe";
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        return node;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
