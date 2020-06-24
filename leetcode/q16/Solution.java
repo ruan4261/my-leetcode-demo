c095049cb3f96b5f33c2aaf3a02cd4cf55dcf109
ruan4261 <ruan4261@foxmail.com>
2020-06-24 16:43:54 +0800

    20-6-24

---
+++ leetcode/q16/Solution.java
@@ -0,0 +1,64 @@
+package com.ruan.alg.leetcode.leetcode.q16;
+
+import java.util.Arrays;
+
+/**
+ * 16. 最接近的三数之和
+ * <p>
+ * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：nums = [-1,2,1,-4], target = 1
+ * 输出：2
+ * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 3 <= nums.length <= 10^3
+ * -10^3 <= nums[i] <= 10^3
+ * -10^4 <= target <= 10^4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum-closest
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ruan4261
+ */
+public class Solution {
+
+    public int threeSumClosest(int[] nums, int target) {
+        int len = nums.length;
+        if (len == 3) return nums[0] + nums[1] + nums[2];
+        Arrays.sort(nums);
+        if (target <= nums[0] + nums[1] + nums[2]) return nums[0] + nums[1] + nums[2];
+        else if (target >= nums[len - 1] + nums[len - 2] + nums[len - 3])
+            return nums[len - 1] + nums[len - 2] + nums[len - 3];
+
+
+        int rec = Integer.MAX_VALUE;
+        int ans = 0;
+        for (int i = 0, l = len - 2; i < l; i++) {
+            int tar = target - nums[i];
+            int head = i + 1, tail = len - 1;
+            while (head < tail) {
+                int curr = nums[head] + nums[tail];
+                if (curr == tar) return target;
+
+                int x = Math.abs(curr - tar);
+                if (x < rec) {
+                    rec = x;
+                    ans = nums[i] + curr;
+                }
+
+                if (curr > tar) tail--;
+                else head++;
+            }
+        }
+        return ans;
+    }
+
+}
---
+++ leetcode/q622/MyCircularQueue.java
@@ -0,0 +1,126 @@
+package com.ruan.alg.leetcode.leetcode.q622;
+
+/**
+ * 622. 设计循环队列
+ * <p>
+ * 设计你的循环队列实现。 循环队列是一种线性数据结构，其操作表现基于 FIFO（先进先出）原则并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”。
+ * <p>
+ * 循环队列的一个好处是我们可以利用这个队列之前用过的空间。在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。但是使用循环队列，我们能使用这些空间去存储新的值。
+ * <p>
+ * 你的实现应该支持如下操作：
+ * <p>
+ * MyCircularQueue(k): 构造器，设置队列长度为 k 。
+ * Front: 从队首获取元素。如果队列为空，返回 -1 。
+ * Rear: 获取队尾元素。如果队列为空，返回 -1 。
+ * enQueue(value): 向循环队列插入一个元素。如果成功插入则返回真。
+ * deQueue(): 从循环队列中删除一个元素。如果成功删除则返回真。
+ * isEmpty(): 检查循环队列是否为空。
+ * isFull(): 检查循环队列是否已满。
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * MyCircularQueue circularQueue = new MyCircularQueue(3); // 设置长度为 3
+ * circularQueue.enQueue(1);  // 返回 true
+ * circularQueue.enQueue(2);  // 返回 true
+ * circularQueue.enQueue(3);  // 返回 true
+ * circularQueue.enQueue(4);  // 返回 false，队列已满
+ * circularQueue.Rear();  // 返回 3
+ * circularQueue.isFull();  // 返回 true
+ * circularQueue.deQueue();  // 返回 true
+ * circularQueue.enQueue(4);  // 返回 true
+ * circularQueue.Rear();  // 返回 4
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 所有的值都在 0 至 1000 的范围内；
+ * 操作数将在 1 至 1000 的范围内；
+ * 请不要使用内置的队列库。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/design-circular-queue
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author ruan4261
+ */
+class MyCircularQueue {
+
+    private final int[] value;
+
+    private int head;
+
+    private int length;
+
+    /**
+     * Initialize your data structure here. Set the size of the queue to be k.
+     */
+    public MyCircularQueue(int k) {
+        this.value = new int[k];
+        this.head = 0;
+        this.length = 0;
+    }
+
+    /**
+     * Insert an element into the circular queue. Return true if the operation is successful.
+     */
+    public boolean enQueue(int value) {
+        if (this.isFull()) return false;
+        int idx = head + length++;
+        if (idx >= this.value.length) idx = idx - this.value.length;
+        this.value[idx] = value;
+        return true;
+    }
+
+    /**
+     * Delete an element from the circular queue. Return true if the operation is successful.
+     */
+    public boolean deQueue() {
+        if (this.isEmpty()) return false;
+        this.length = this.length - 1;
+        if (++this.head == this.value.length) head = 0;
+        return true;
+    }
+
+    /**
+     * Get the front item from the queue.
+     */
+    public int Front() {
+        return this.isEmpty() ? -1 : this.value[this.head];
+    }
+
+    /**
+     * Get the last item from the queue.
+     */
+    public int Rear() {
+        if (this.isEmpty()) return -1;
+        int idx = this.head + this.length - 1;
+        if (idx >= this.value.length) idx = idx - this.value.length;
+        return this.value[idx];
+    }
+
+    /**
+     * Checks whether the circular queue is empty or not.
+     */
+    public boolean isEmpty() {
+        return this.length == 0;
+    }
+
+    /**
+     * Checks whether the circular queue is full or not.
+     */
+    public boolean isFull() {
+        return this.length == this.value.length;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
