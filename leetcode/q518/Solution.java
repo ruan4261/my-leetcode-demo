1e01f3cc7de190821c10a4c64b6e739caaad6d37
ruan4261 <i@4261.ink>
2021-06-10 21:08:50 +0800

    21-06-10 6月10日 阴 被🦟咬了, dnmdp

---
+++ leetcode/q518/Solution.java
@@ -0,0 +1,19 @@
+package demo.leetcode.q518;
+
+public class Solution {
+
+    /**
+     * dnm
+     */
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+
+}
